21896c3b1623937818ec1c45c306d39e97645596
Jay <jay@wjdonaldson.com>
2023-08-08 13:41:50 -0700

    Adds our models into admin.py and adds the list and details views for Destinations

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('destinations/', views.DestinationList.as_view(), name='destinations_list'),
+  path('destinations/<int:destination_id>/', views.destinations_detail, name='detail'),
+
   path('accounts/signup/', views.signup, name='signup'),
 ]
