6ae0226e479db36a55ab7daebb568dde50841d43
priyankanair1 <priyankanairammu90@gmail.com>
2023-08-09 00:02:19 -0700

    Add Destinations

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
@@ -72,6 +75,20 @@ class CommentDelete(DeleteView):
   model = Comment
   success_url = '/destinations'
 
+def add_photo(request, destination_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            Photo.objects.create(url=url, destination_id=destination_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('detail', destination_id=destination_id)
 
 def signup(request):
   error_message = ''
