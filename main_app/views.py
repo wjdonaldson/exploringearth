fc06a989e8ae2daa7d13581a65d7ae5f026834e7
priyankanair1 <priyankanairammu90@gmail.com>
2023-08-10 08:41:46 -0700

    update readme

--- main_app/views.py
+++ main_app/views.py
@@ -83,7 +83,6 @@ class CommentDetail(LoginRequiredMixin, DetailView):
     model = Comment
     fields = ["comment", "timestamp", "user", "destination"]
 
-
 class CommentCreate(LoginRequiredMixin, CreateView):
     model = Comment
     fields = ["comment"]
