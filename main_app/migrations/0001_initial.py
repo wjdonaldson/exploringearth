c4e9a4faa3d3a2dd403e968b1454cbc8191aa967
Jay <jay@wjdonaldson.com>
2023-08-11 09:26:41 -0700

    re-add migration files that somehow got lost

--- main_app/migrations/0001_initial.py
+++ main_app/migrations/0001_initial.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-class Migration(migrations.Migration):
+class Migration(migrations.Migration): 
 
     initial = True
 
--- main_app/migrations/0002_destination_timestamp_photo_caption.py
+++ main_app/migrations/0002_destination_timestamp_photo_caption.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="destination",
             name="timestamp",
-            field=models.DateTimeField(auto_now=True),
+            field=models.DateTimeField(auto_now=True), 
         ),
         migrations.AddField(
             model_name="photo",
