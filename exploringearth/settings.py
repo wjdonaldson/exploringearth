5be1dc853b8e2fa84975afdfbc0f02f5898a2cc6
Jay <jay@wjdonaldson.com>
2023-08-11 20:26:08 -0700

    remove heroku settings

--- exploringearth/settings.py
+++ exploringearth/settings.py
@@ -85,12 +85,12 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'exploringearth',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': os.environ['DATABASE_SERVER'],
-        'PORT': '5432',
+        'NAME': 'exploringearth',
+        # 'NAME': os.environ['DATABASE_NAME'],
+        # 'USER': os.environ['DATABASE_USER'],
+        # 'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        # 'HOST': os.environ['DATABASE_SERVER'],
+        # 'PORT': '5432',
     }
 }
 
@@ -145,6 +145,6 @@ LOGOUT_REDIRECT_URL = "/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # For deploying Django on Heroku
-import django_on_heroku
-django_on_heroku.settings(locals())
+# import django_on_heroku
+# django_on_heroku.settings(locals())
 DATABASES['default']['CONN_MAX_AGE'] = 0
